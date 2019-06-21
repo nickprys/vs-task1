13e0e0094986111da2dcc88d721bff61ec579a2a
nickprys <nickprys@gmail.com>
2019-06-21 15:29:12 +0300

    no message

---
+++ ConsolePrj1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace ConsolePrj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Surname: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("You are {0} {1}", name, surname);
+            Console.WriteLine("It's project ConsolPrj1");
+            Console.Write("Hello {0}",name);
+            string surnam = Console.ReadLine();
+        }
+    }
+}
